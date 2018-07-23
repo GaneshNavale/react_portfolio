ed058e3eaa5598ae6382cbb6a0cabc7687df6dba
Ganesh Navale <ganesh@edcast.com>
2018-07-23 22:52:49 +0530

    added different components

---
+++ src/components/Portfolio.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Portfolio extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <div> <h1>Portfolio Page</h1> </div> );
+    }
+}
+ 
+export default Portfolio;
